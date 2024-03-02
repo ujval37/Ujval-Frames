36236db8e1025cca2b66feb30fcd315e7e026ecc
ujval37 <122030386+ujval37@users.noreply.github.com>
2024-03-02 18:01:58 +0100

    newversion

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// Import necessary modules
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
+// Create HTTP server
 const server = http.createServer((req, res) => {
   // GET / (Index Route)
+  // Return a frame which renders an image with four redirect buttons
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(`
@@ -14,7 +17,7 @@ const server = http.createServer((req, res) => {
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Farcaster Frame Example</title>
           <meta name="fc:frame" content="vNext">
-          <meta name="fc:frame:image" content="https://fc-dev-call.replit.app/image">
+          <meta name="fc:frame:image" content="https://raw.githubusercontent.com/ujval37/Ujval-Frames/main/frame-fc.png">
       </head>
       <body>
           <div id="frameContainer" style="width: 600px; height: 400px;">
@@ -30,18 +33,22 @@ const server = http.createServer((req, res) => {
     res.end();
   } else if (req.url === "/image") {
     // GET /image
+    // Return the image used in the image tag
     const imagePath = path.join(__dirname, "frame-fc.png");
     const imageStream = fs.createReadStream(imagePath);
     res.writeHead(200, { "Content-Type": "image/png" });
     imageStream.pipe(res);
   } else {
+    // Catchall 404 Route
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Page not found");
   }
 });
 
+// Define the port
 const port = process.env.PORT || 3000;
 
+// Start the server
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
