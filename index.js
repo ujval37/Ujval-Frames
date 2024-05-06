58d0fa6bfbec55f1b2a5e244e7ecbe2b0a51b065
ujval37 <122030386+ujval37@users.noreply.github.com>
2024-05-06 23:25:19 +0200

    jusupdate

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-// Import necessary modules
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-// Create HTTP server
 const server = http.createServer((req, res) => {
-  // GET / (Index Route)
-  // Return the HTML file
   if (req.url === "/") {
-    const htmlFilePath = path.join(__dirname, "index.html");
-    fs.readFile(htmlFilePath, "utf-8", (err, data) => {
-      if (err) {
-        console.error("Error reading HTML file:", err);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("500 Internal Server Error");
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      }
-    });
+    // Serve HTML content
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(path.join(__dirname, "index.html")).pipe(res);
+  } else if (req.url === "/image") {
+    // Serve image file
+    const imagePath = path.join(__dirname, "Lynx_HeadOn_Crop1.jpg");
+    const imageStream = fs.createReadStream(imagePath);
+    res.writeHead(200, { "Content-Type": "image/jpeg" }); // Specify correct content type for the image
+    imageStream.pipe(res);
   } else {
-    // For all other requests, return a 404 Not Found error
+    // Handle 404 Not Found
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("404 Not Found");
   }
 });
 
-// Define the port
 const port = process.env.PORT || 3000;
-
-// Start the server
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
